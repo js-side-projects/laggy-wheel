772a69695913cc218907799da3759c952fc6b96c
connoringold <connoringold@gmail.com>
2021-04-12 18:03:08 +0100

    added vuex and on click example

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex'
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+	state: {
+		data: 2,
+	},
+	mutations: {},
+	actions: {},
+	modules: {},
+})
---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<h1>Msg:</h1>
+		<Hello />
+		<p>Vuex (Global Data/state): {{ store.state }}</p>
+		<p>Number (Local Data/state) {{ number }}</p>
+		<button @click="update">Hello</button>
+	</div>
+</template>
+
+<script>
+	import { ref } from 'vue'
+	import { useStore } from 'vuex'
+	import Hello from '../components/Hello.vue'
+	export default {
+		components: {
+			Hello,
+		},
+		setup() {
+			const store = useStore()
+
+			const number = ref(1)
+
+			const update = () => {
+				number.value = 2
+			}
+
+			return { Hello, number, store, update }
+		},
+	}
+</script>
